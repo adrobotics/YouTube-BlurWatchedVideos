a2c9c298e98901812324a404b5d066714a86a9e7
Adrian Popa <ad_robotics@yahoo.com>
2018-06-28 15:09:06 -0700

    Create youtube-blur-watched-videos.meta.js

---
+++ youtube-blur-watched-videos.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         YouTube-BlurWatchedVideos
+// @author       Adrian Popa (ad_robotics@yahoo.com)
+// @namespace    https://github.com/adrobotics/YouTube-BlurWatchedVideos
+// @description  Blurs the thumbnail of all previously watched videos on YouTube.
+// @version      1.0
+// @downloadURL  https://raw.githubusercontent.com/adrobotics/YouTube-BlurWatchedVideos/blob/master/youtube-blur-watched-videos.user.js
+// @updateURL    https://raw.githubusercontent.com/adrobotics/YouTube-BlurWatchedVideos/blob/master/youtube-blur-watched-videos.meta.js
+// @icon         https://www.google.com/s2/favicons?domain=www.youtube.com
+// @match        https://www.youtube.com
+// @match        https://www.youtube.com/watch*
+// @match        https://www.youtube.com/feed/*
+// @run-at       document-end
+// ==/UserScript==
